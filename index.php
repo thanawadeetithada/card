feca6c2b8d9fdbe22fd7a3e8edc5a2178e315446
Baifern <thanawadee.titha@gmail.com>
2025-11-18 22:21:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="th">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-title" content="App Premium">
+    <meta name="application-name" content="App Premium">
+    <meta name="theme-color" content="#96a1cd">
+    <title>เมนู</title>
+    <link rel="manifest" href="manifest.json">
+    <link rel="apple-touch-icon" href="icons/icon-192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="icons/icon-192.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <style>
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: 'Prompt', sans-serif;
+        background: url('bg/sky.png') no-repeat center center;
+        background-size: cover;
+        background-attachment: fixed;
+        min-height: 100vh;
+        padding-bottom: 50px;
+    }
+
+    .navbar {
+        padding: 20px;
+    }
+
+    .menu-card {
+        background: rgba(255, 255, 255, 0.9);
+        border-radius: 15px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        padding: 10px;
+        margin-bottom: 20px;
+        transition: 0.3s;
+        max-width: 350px;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 250px;
+    }
+
+    .menu-card:hover {
+        transform: translateY(-3px);
+    }
+
+    .menu-title {
+        font-weight: 600;
+        color: #d32f2f;
+    }
+
+    .header-img {
+        width: 90%;
+        height: auto;
+        border-radius: 15px;
+        margin-top: 10px;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.15);
+    }
+
+    @media (max-width: 768px) {
+        .menu-title {
+            font-size: 1rem;
+        }
+    }
+
+
+    .position-relative {
+        display: inline-block;
+    }
+
+    .btn-danger i {
+        color: white;
+        font-size: 1.1rem;
+    }
+
+    .btn-danger:hover {
+        background-color: #c62828;
+    }
+    </style>
+</head>
+
+<body>
+  <div class="container">  
+<div class="valentines">
+  <div class="envelope"></div>
+  <div class="front"></div>
+  <div class="card">
+  <div class="text">Happy</br> Valentine's</br> Day!</div>
+  <div class="heart"</div>
+  </div>
+  <div class="hearts">
+    <div class="one"></div>
+    <div class="two"></div>
+    <div class="three"></div>
+    <div class="four"></div>
+    <div class="five"></div>
+  </div>
+</div>
+</div>
+<div class="shadow"></div>
+</div>
+</body>
+
+</html>
